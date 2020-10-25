0df28e80d3182c25d0b3597a2fd3d862b1eb1604
ak <achyut.kadam@definelabs.com>
2020-10-25 16:48:26 +0530

    added validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ApplicationRecord
-
+  validates :name, :state, :district, presence: true
+  validates :name, numericality: false
   def address
     puts "============="
     p self.state+ ' ' +self.district
