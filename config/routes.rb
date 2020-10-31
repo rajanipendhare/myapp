781e58afe746d6bcc2f2786df3bc5fe429e578a2
rajanipendhare <rbwakchaure15@gmail.com>
2020-10-31 12:39:21 +0530

    Removed unneccesary login form

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
   post '/login', controller: :users, action: :login
   delete '/logout', controller: :users, action: :logout
   root 'users#new'
-	  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-	# root 'home#index'
 	get 'home/dashboard'
 	get 'home/SignUp'
-	get 'home/index'
 	get 'home/table'
 end
